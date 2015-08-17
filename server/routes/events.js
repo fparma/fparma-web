63e49f2fc6dd09b846f297992a282aa66791123e
Cuel <alleee@gmail.com>
2015-08-17 17:35:50 +0200

    work on events

--- server/routes/events.js
+++ server/routes/events.js
@@ -1,6 +1,13 @@
 import {Router} from 'express'
+import moment from 'moment'
 
 const router = Router()
 export default router
 
-//router.get('/')
+router.get('/events', (req, res) => {
+  res.render('events/list.jade', {events: {upcoming: [], completed: []}})
+})
+
+router.get('/event/:permalink', (req, res) => {
+  res.redirect('/events')
+})
