1949fa1ff9802a5dafdbf6dcbbc4886ce1c87135
Cuel <alleee@gmail.com>
2016-03-08 14:05:52 +0100

    refactor some routes to standalone files

---
+++ server/routes/guides.js
@@ -0,0 +1,19 @@
+import fs from 'fs'
+import {files as userGuides} from '../utils/generate-contrib-markdown'
+
+export default function (req, res, next) {
+  let id = req.url
+  let guide = userGuides.find(v => v.url === id)
+  if (!guide) return next()
+
+  // TODO: makes this better. use cache
+  fs.readFile(guide.file, 'utf8', (err, data) => {
+    if (err) return next() // 404
+
+    res.render('user-guides/guide.jade', {
+      title: guide.title,
+      topMenu: 'guides',
+      guide: data
+    })
+  })
+}
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,17 +1,13 @@
-import fs from 'fs'
 import {Router} from 'express'
-import nconf from 'nconf'
-import _ from 'lodash'
-import builder from 'xmlbuilder'
 
 import auth from './auth'
 import events from './events'
 import profile from './profile'
 import media from './media'
 import admin from './admin'
+import squadXml from './squad-xml'
+import guides from './guides'
 import News from '../controllers/news'
-import User from '../controllers/user'
-import {files as userGuides} from '../utils/generate-contrib-markdown'
 
 export const router = Router()
 
@@ -36,60 +32,13 @@ router.use('/media', media)
 router.use('/events', events)
 router.use('/admin', admin)
 
+router.get('/guides/:id', guides)
+router.get('/squad.xml', squadXml)
+
 router.get('/about', (req, res) => {
   res.render('about.jade', {page: 'about', title: 'About'})
 })
 
 router.get('/policy', (req, res) => {
   res.render('policy.jade', {tite: 'Policy'})
-})
-
-router.get('/guides/:id', (req, res, next) => {
-  let id = req.url
-  let guide = userGuides.find(v => v.url === id)
-  if (!guide) return next()
-
-  // TODO: makes this better. use cache
-  fs.readFile(guide.file, 'utf8', (err, data) => {
-    if (err) return next() // 404
-
-    res.render('user-guides/guide.jade', {
-      title: guide.title,
-      topMenu: 'guides',
-      guide: data
-    })
-  })
-})
-
-router.get('/squad.xml', (req, res, next) => {
-  User.getUnitsForXml((err, users) => {
-    if (err) return next(err)
-    var root = builder.create('squad')
-    root.dtd('squad.dtd')
-
-    _.forOwn(nconf.get('SQUAD_XML'), (v, key) => {
-      if (key === 'nick') return root.att(key, v)
-      root.ele(key, v)
-    })
-
-    users.forEach((v) => {
-      root.ele({
-        member: {
-          '@id': v.steam_id,
-          '@nick': v.squad.nick,
-          name: 'N/A',
-          email: 'N/A',
-          icq: 'N/A',
-          remark: v.squad.remark
-        }
-      })
-    })
-
-    var xmlString = root.end({pretty: true})
-    res.set('Content-Type', 'application/xml')
-    res.set('Cache-Control', 'private, no-cache, no-store, must-revalidate')
-    res.set('Expires', '-1')
-    res.set('Pragma', 'no-cache')
-    res.send(xmlString)
-  })
 })
