f783ca2620a040751eece90b05e400fd806c09c7
Cuel <alleee@gmail.com>
2016-03-10 20:28:58 +0100

    recursive ts3 channels

--- server/routes/a3-server-info.js
+++ server/routes/a3-server-info.js
@@ -83,7 +83,14 @@ function getTS3Info () {
         return resolve([])
       }
 
-      let channels = d.raw.channels.filter(v => v.pid === root.cid).map(v => v.cid)
+      let channels = [root]
+      let subChannels = d.raw.channels.filter(v => v.pid === root.cid).map(v => v.cid)
+      let recurse = id => {
+        channels.push(id)
+        d.raw.channels.filter(v => v.pid === id).forEach(v => recurse(v.cid))
+      }
+      subChannels.forEach(recurse)
+
       let clients = d.players.filter(v => channels.indexOf(v.cid) !== -1).map(v => v.name)
       resolve(clients)
     })
