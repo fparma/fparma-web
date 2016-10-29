f2c5f0fd501e8be63ba0e609f1fd28cddabad6d3
Cuel <alleee@gmail.com>
2016-10-29 13:49:34 +0200

    Make sure message exists

--- server/routes/error-handler.js
+++ server/routes/error-handler.js
@@ -13,12 +13,13 @@ export function handleError (IS_DEV) {
       if (req.sesssion) req.session.destroy()
     }
 
+    const msg = (err.message || 'Unknown error')
     res.status(err.status || req.xhr && err.status !== 404 ? 200 : 500)
-    if (err.status !== 404) console.error(err, err.stack.split('\n'))
-    if (req.xhr) return res.json({ok: false, error: err.message})
+    if (err.status !== 404) console.error(err, (err.stack || '').split('\n'))
+    if (req.xhr) return res.json({ok: false, error: msg})
     res.render('error', {
       title: err.status === 404 ? 'Not found' : 'Oops!',
-      message: err.message,
+      message: msg,
       error: IS_DEV ? err : null
     })
   }
