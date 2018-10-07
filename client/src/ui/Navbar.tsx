d60b07bd07c16229d3869ba8578eb0beb9d8fc0b
Cuel <Cuel@users.noreply.github.com>
2018-10-07 15:41:30 +0200

    fix outside click

--- client/src/ui/Navbar.tsx
+++ client/src/ui/Navbar.tsx
@@ -4,45 +4,70 @@ import { printClass } from './utils'
 
 interface IProps {
   className?: string
-  modifiers?: string[]
   children: React.ReactNode
 }
 
-type BurgerProps = { className?: string; onClick: (event: React.MouseEvent<HTMLAnchorElement>) => void }
-const Burger: React.SFC<BurgerProps> = props => (
-  <a
-    role="button"
-    tabIndex={0}
-    onClick={props.onClick}
-    className={printClass('navbar-burger', props.className)}
-    aria-label="menu"
-    aria-expanded="false"
-  >
-    <span aria-hidden="true" />
-    <span aria-hidden="true" />
-    <span aria-hidden="true" />
-  </a>
+const Context = React.createContext<{ mobileMenuOpen: boolean }>({ mobileMenuOpen: false })
+
+const Menu = (props: { children: { start: React.ReactNode; end: React.ReactNode }; className: string }) => (
+  <Context.Consumer>
+    {state => (
+      <div className={printClass('navbar-menu', props.className, { 'is-active': state.mobileMenuOpen })}>
+        <div className="navbar-start">{props.children.start}</div>
+        <div className="navbar-end">{props.children.end}</div>
+      </div>
+    )}
+  </Context.Consumer>
 )
 
-const Main: React.SFC<IProps> = ({ className, modifiers = [], children }) => (
-  <nav className={printClass('navbar', className, ...modifiers)} role="navigation" aria-label="main navigation">
-    {children}
-  </nav>
+interface MainProps extends IProps {
+  hasShadow: boolean
+  mobileMenuOpen: boolean
+}
+
+const Main: React.SFC<MainProps> = ({ className, hasShadow, children, mobileMenuOpen }) => (
+  <Context.Provider value={{ mobileMenuOpen }}>
+    <nav
+      className={printClass('navbar', className, { 'has-shadow': hasShadow })}
+      role="navigation"
+      aria-label="main navigation"
+    >
+      {children}
+    </nav>
+  </Context.Provider>
 )
 
-const Brand = (props: any) => <div className="navbar-brand">{props.children}</div>
+interface BurgerProps {
+  className?: string
+  onClick: (event: React.MouseEvent<HTMLAnchorElement>) => void
+}
+
+const Burger = React.forwardRef((props: BurgerProps, ref) => (
+  <Context.Consumer>
+    {state => (
+      <a
+        ref={ref as any}
+        role="button"
+        tabIndex={0}
+        onClick={props.onClick}
+        className={printClass('navbar-burger', props.className, { 'is-active': state.mobileMenuOpen })}
+        aria-label="menu"
+        aria-expanded="false"
+      >
+        <span aria-hidden="true" />
+        <span aria-hidden="true" />
+        <span aria-hidden="true" />
+      </a>
+    )}
+  </Context.Consumer>
+))
+
+const Brand = (props: IProps) => <div className={printClass('navbar-brand', props.className)}>{props.children}</div>
 
 const Item = (props: LinkProps) => (
   <Link className="navbar-item" {...props}>
     {props.children}
   </Link>
-)
-
-const Menu = (props: { children: { start: React.ReactNode; end: React.ReactNode }; className: string }) => (
-  <div className={printClass('navbar-menu', props.className)}>
-    <div className="navbar-start">{props.children.start}</div>
-    <div className="navbar-end">{props.children.end}</div>
-  </div>
 )
 
 export const Navbar = {
