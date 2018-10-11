e78e9afd68acedef460e13fc033c64b4302cc1c0
Cuel <Cuel@users.noreply.github.com>
2018-10-12 00:39:57 +0200

    input

--- client/src/ui/Input.tsx
+++ client/src/ui/Input.tsx
@@ -1,10 +1,54 @@
 import * as React from 'react'
+import { Icon, ICONS } from './Icon'
 import { printClass } from './utils'
 
 interface IInputProps {
-  className: string
+  className?: string
 }
 
-export const Input: React.SFC<IInputProps> = ({className}) => {
+export const Input: React.SFC<IInputProps> = ({ className }) => {
   return <input type="text" className={printClass('input', className)} />
+}
+
+interface ICheckboxProps extends IInputProps {
+  checked?: boolean | undefined
+  onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void
+}
+
+export class Checkbox extends React.PureComponent<ICheckboxProps, { checked: boolean | undefined }> {
+  inpRef: React.RefObject<HTMLInputElement>
+
+  constructor(props: ICheckboxProps) {
+    super(props)
+    this.state = {
+      checked: props.checked,
+    }
+  }
+
+  onChange(e: React.ChangeEvent<HTMLInputElement>) {
+    this.setState(prevState => {
+      checked: !prevState.checked
+    })
+    if (this.props.onChange) this.props.onChange(e)
+  }
+
+  render() {
+    const { className } = this.props
+    return (
+      <div className="field">
+        <label>
+          <input
+            ref={this.inpRef}
+            type="checkbox"
+            onChange={e => this.onChange(e)}
+            className={printClass('is-checkbox', className)}
+          />
+          {this.state.checked && <Icon icon={ICONS.faCheckSquare} />}
+          {!this.state.checked && <Icon icon={ICONS.faSquareFull} />}
+
+          <span>Test</span>
+        </label>
+      </div>
+    )
+  }
 }
